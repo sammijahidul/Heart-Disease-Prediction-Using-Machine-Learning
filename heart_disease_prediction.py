9b19a0cfb2e980fbf51e509c029680137d249c8a
sammijahidul <jahidul.islam03@northsouth.edu>
2021-10-31 23:22:07 +0600

    cheking the size of the data and what types of data are their in dataset

--- heart_disease_prediction.py
+++ heart_disease_prediction.py
@@ -5,3 +5,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
+## Data preprocessing part start from here
+# Import the dataset to pandas dataframe
+data = pd.read_csv('heart.csv')
+data.head()
+data.tail()
+data.shape
