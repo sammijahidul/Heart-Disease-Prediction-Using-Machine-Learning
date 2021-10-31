58e33ea7a9a20b2f09f51aa30e9052d79cfc3e92
sammijahidul <jahidul.islam03@northsouth.edu>
2021-10-31 23:37:49 +0600

    splitting the dataset into input and output

--- heart_disease_prediction.py
+++ heart_disease_prediction.py
@@ -8,6 +8,18 @@ from sklearn.metrics import accuracy_score
 ## Data preprocessing part start from here
 # Import the dataset to pandas dataframe
 data = pd.read_csv('heart.csv')
+
+# Check the data information 
 data.head()
 data.tail()
 data.shape
+data.info()
+data.isnull().sum()
+data.describe()
+data['target'].value_counts()
+
+# Splitting the output and feature columns[input]
+X = data.drop(columns = 'target', axis = 1)
+Y = data['target']
+print(X)
+print(Y)
