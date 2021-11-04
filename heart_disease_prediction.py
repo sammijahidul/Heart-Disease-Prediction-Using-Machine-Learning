9c17868ec71a5167e7ed799ec02b4550a7402e55
sammijahidul <jahidul.islam03@northsouth.edu>
2021-11-04 23:38:48 +0600

    Data pre-processing done building the model starts from here

--- heart_disease_prediction.py
+++ heart_disease_prediction.py
@@ -23,3 +23,10 @@ X = data.drop(columns = 'target', axis = 1)
 Y = data['target']
 print(X)
 print(Y)
+
+# Splitting the dataset into training set and test set
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y,
+                                                    random_state=2)
+print(X.shape, X_train.shape, X_test.shape)
+
+## Started to build the model from here
